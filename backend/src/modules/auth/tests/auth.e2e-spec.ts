15eb36e4e15cbd399d662c9e699a68aa296faf7d
JesseWilliams1642 <165112869+JesseWilliams1642@users.noreply.github.com>
2026-03-06 10:25:51 +1100

    Finished auth module e2e tests

--- backend/src/modules/auth/tests/auth.e2e-spec.ts
+++ backend/src/modules/auth/tests/auth.e2e-spec.ts
@@ -28,37 +28,6 @@ describe("Auth Module (e2e)", () => {
 		app.use(cookieParser());
 		await app.init();
 
-		// Register test user
-		const registerRes = await request(app.getHttpServer())
-			.post("/api/auth/register")
-			.send(TEST_USER);
-
-		if (registerRes.status !== 201) {
-			throw new Error(
-				`Failed to register test user. Status: ${registerRes.status}, Body: ${JSON.stringify(registerRes.body)}`,
-			);
-		}
-
-		// Login and capture JWT token
-		const loginRes = await request(app.getHttpServer())
-			.post("/api/auth/login")
-			.send({
-				email: TEST_USER.email,
-				password: TEST_USER.password,
-			});
-
-		if (loginRes.status !== 200) {
-			throw new Error(
-				`Failed to log in test user. Status: ${loginRes.status}, Body: ${JSON.stringify(loginRes.body)}`,
-			);
-		}
-
-		const setCookieHeader = loginRes.headers["set-cookie"]?.[0];
-		if (!setCookieHeader) throw new Error("No Set-Cookie header in login response");
-
-		authToken = setCookieHeader.split(";")[0].split("=")[1];
-		if (!authToken)
-			throw new Error("Failed to extract JWT token from Set-Cookie header");
 	});
 
 	// Remove test user after tests are completed
@@ -79,11 +48,254 @@ describe("Auth Module (e2e)", () => {
 		}
 	});
 
-	describe("POST api/auth/login", () => {});
+	describe("POST api/auth/register", () => {
+		it("should return the created user for a successful registration", async () => {
+			const registerRes = await request(app.getHttpServer())
+				.post("/api/auth/register")
+				.send(TEST_USER)
+				.expect(201);
 
-	describe("POST api/auth/logout", () => {});
+			expect(registerRes.body).toHaveProperty("data");
+			expect(registerRes.body).toHaveProperty("error");
+			const createdUser = registerRes.body.data;
+			expect(registerRes.body.error).toBeNull;
 
-	describe("POST api/auth/register", () => {});
+			expect(createdUser).toHaveProperty("email", TEST_USER.email);
+			expect(typeof createdUser.email).toBe("string");
+			expect(createdUser.email.length).toBeGreaterThan(0);
+			
+			expect(createdUser).toHaveProperty("id");
+			expect(typeof createdUser.id).toBe("string");
+			expect(createdUser.id.length).toBeGreaterThan(0);
+			
+			expect(createdUser).toHaveProperty("name", TEST_USER.name);
+			expect(typeof createdUser.name).toBe("string");
+			expect(createdUser.name.length).toBeGreaterThan(0);
+		});
 
-	describe("GET api/auth/me", () => {});
+		it("should throw an error if the user already exists", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/register")
+				.send(TEST_USER)
+				.expect(400);
+		});
+
+		it("should throw an error for an empty email field", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/register")
+				.send({
+					name: TEST_USER.name,
+					password: TEST_USER.password
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for an invalid email format", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/register")
+				.send({
+					email: "thisisnotanemail",
+					name: TEST_USER.name,
+					password: TEST_USER.password
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for an empty password field", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/register")
+				.send({
+					email: TEST_USER.email,
+					name: TEST_USER.name,
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for a non-string password field", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/register")
+				.send({
+					email: TEST_USER.email,
+					name: TEST_USER.name,
+					password: 1234
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for passwords of length 7 or less", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/register")
+				.send({
+					email: TEST_USER.email,
+					name: TEST_USER.name,
+					password: "uhoh"
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for an empty name field", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/register")
+				.send({
+					email: TEST_USER.email,
+					password: TEST_USER.password
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for a non-string name field", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/register")
+				.send({
+					email: TEST_USER.email,
+					name: 1234,
+					password: TEST_USER.password
+				})
+				.expect(400);
+		});
+
+	});
+
+	describe("POST api/auth/login", () => {
+		it("should send a JWT token for the correct login credentials", async () => {
+			// Login and capture JWT token
+			const loginRes = await request(app.getHttpServer())
+				.post("/api/auth/login")
+				.send({
+					email: TEST_USER.email,
+					password: TEST_USER.password,
+				})
+				.expect(200);
+
+			const setCookieHeader = loginRes.headers["set-cookie"]?.[0];
+			authToken = setCookieHeader.split(";")[0].split("=")[1];
+			
+			expect(typeof authToken).toBe("string");
+			expect(authToken.length).toBeGreaterThan(0);
+
+			expect(loginRes.body).toHaveProperty("data", "Logged out successfully.");
+			expect(loginRes.body).toHaveProperty("error", null);
+		});
+
+		it("should throw an error for a non-registered email", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/login")
+				.send({
+					email: "thisisnotregisteredtest123@email.com",
+					password: TEST_USER.password,
+				})
+				.expect(404);
+		});
+
+		it("should throw an error for an incorrect password", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/login")
+				.send({
+					email: TEST_USER.email,
+					password: "thisisdefinitelyincorrect",
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for an empty email field", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/login")
+				.send({
+					password: TEST_USER.password,
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for an invalid email field", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/login")
+				.send({
+					email: "notanemail",
+					password: TEST_USER.password,
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for empty password field", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/login")
+				.send({
+					email: TEST_USER.email,
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for a password of 7 characters or less", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/login")
+				.send({
+					email: TEST_USER.email,
+					password: "uhoh",
+				})
+				.expect(400);
+		});
+
+		it("should throw an error for a non-string password field", async () => {
+			await request(app.getHttpServer())
+				.post("/api/auth/login")
+				.send({
+					email: TEST_USER.email,
+					password: 1234,
+				})
+				.expect(400);
+		});
+
+	});
+
+	describe("GET api/auth/me", () => {
+		it("should return user information for valid authenticated JWT token", async () => {
+			const registerRes = await request(app.getHttpServer())
+				.post("/api/auth/me")
+				.set("Cookie", getCookieHeader(authToken))
+				.expect(200);
+
+			expect(registerRes.body).toHaveProperty("data");
+			expect(registerRes.body).toHaveProperty("error");
+			const returnedUser = registerRes.body.data;
+			expect(registerRes.body.error).toBeNull;
+
+			expect(returnedUser).toHaveProperty("email", TEST_USER.email);
+			expect(typeof returnedUser.email).toBe("string");
+			expect(returnedUser.email.length).toBeGreaterThan(0);
+			
+			expect(returnedUser).toHaveProperty("id");
+			expect(typeof returnedUser.id).toBe("string");
+			expect(returnedUser.id.length).toBeGreaterThan(0);
+			
+			expect(returnedUser).toHaveProperty("name", TEST_USER.name);
+			expect(typeof returnedUser.name).toBe("string");
+			expect(returnedUser.name.length).toBeGreaterThan(0);
+		});
+
+		it("should return 401 when accessing without authentication", async () => {
+			await request(app.getHttpServer()).get("/api/auth/me").expect(401);
+		});
+		
+		it("should return 401 with invalid JWT token", async () => {
+			await request(app.getHttpServer())
+				.get("/api/auth/me")
+				.set("Cookie", getCookieHeader("invalid-token"))
+				.expect(401);
+		})
+	});
+
+	describe("POST api/auth/logout", () => {
+		it("should remove the JWT auth token", async () => {
+			const logoutRes = await request(app.getHttpServer())
+				.post("/api/auth/logout")
+				.expect(200);
+
+			const setCookieHeader = logoutRes.headers["set-cookie"]?.[0];
+			expect(setCookieHeader).toBeNull();
+
+			expect(logoutRes.body).toHaveProperty("data", "Logged out successfully.");
+			expect(logoutRes.body).toHaveProperty("error", null);
+		});
+	});
+
 });
